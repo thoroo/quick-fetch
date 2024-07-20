46380ad923380c0ba84095b835690f9e2f95cd87
Thomas Roosdorp <thomas@roosdorp.se>
2024-07-20 14:24:38 +0200

    Move code from main for better clarity

--- quick_fetch/__main__.py
+++ quick_fetch/__main__.py
@@ -1,8 +1,69 @@
+import logging
+import os
 from colorama import Fore
 import quick_fetch
+from quick_fetch.hotkeys import register_hotkeys
 from quick_fetch.main import run
 from quick_fetch import logger
+from quick_fetch.configuration import load_config
+from quick_fetch.driver import create_driver
+from quick_fetch.exit_handler import clean_exit
+from tempfile import TemporaryDirectory
+from quick_fetch import constants as const
+from colorama import init
+import chime
 
 if __name__ == '__main__':
+    """Entry point of the application. All setup is done here first."""
+
     logger.info(f"{Fore.CYAN}QuickFetch{Fore.WHITE} {quick_fetch.__version__}")
-    run()
+
+    global CONFIG
+    global TEMP_DIR
+    global DRIVER
+    global USE_SOUND
+
+    CONFIG = load_config()
+    TEMP_DIR = TemporaryDirectory()
+    DRIVER = create_driver(TEMP_DIR)
+
+    # update output path if using OS path variables, specifically %USERPROFILE%
+    output_dir = CONFIG.read_path(const.KEY_OUTPUT_DIR)
+    output_dir = output_dir.replace("%USERPROFILE%", os.environ.get("USERPROFILE", ""))
+    CONFIG.set(section=const.SECTION_PATH, option=const.KEY_OUTPUT_DIR, value=output_dir)    
+
+    const.RESOURCES_DIR.mkdir(parents=True, exist_ok=True)
+
+    # helps rich logger to properly output colors
+    init(convert=True)
+
+    log_level = CONFIG.read_general(const.KEY_LOG_LEVEL)
+    logging.getLogger("rich").setLevel(log_level)
+    logging.getLogger("selenium.webdriver.remote.remote_connection").setLevel(log_level)
+
+    sound_theme = CONFIG.read_general(const.KEY_SOUND).lower()
+
+    if sound_theme != 'none':
+        try:
+            chime.theme(sound_theme)
+        except Exception:
+            logger.warning(f'{const.KEY_SOUND} is not a valid theme. Defaulting to "big-sur".')
+            chime.theme('big-sur')
+            chime.warning()
+        
+        USE_SOUND = True
+    else:
+        USE_SOUND = False
+        logger.info('Sound has been disabled in config.')
+
+    mode = CONFIG.read_general(const.KEY_MODE).lower()
+    hotkeys = CONFIG.get_section(const.SECTION_HOTKEY)
+    register_hotkeys(hotkeys, mode)    
+
+    logger.info(f"Output path set to: {Fore.GREEN}{output_dir}{Fore.WHITE}")
+    logger.info(f"Using mode: {Fore.GREEN}{mode.title()}{Fore.WHITE}")
+
+    try:
+        run()
+    finally:
+        clean_exit()
--- quick_fetch/main.py
+++ quick_fetch/main.py
@@ -1,77 +1,15 @@
 import time
-from tempfile import TemporaryDirectory
-import os
-import logging
-import atexit
 import keyboard
-import chime
-from colorama import Fore, init
 from quick_fetch import logger
-from quick_fetch import constants as c
-from quick_fetch.configuration import load_config
-from quick_fetch.hotkeys import register_hotkeys
-from quick_fetch.driver import create_driver
-from quick_fetch.exit_handler import clean_exit
 
 def run():
     """Main function to run the application"""
-    # helps rich logger to properly output colors
-    init(convert=True)
-    
-    global MODE
-    global CONFIG
-    global PATH_OUTPUT
-    global PATH_TEMP
-    global DRIVER
-    global USE_SOUND
+    try:
+        while True:
+            time.sleep(0.5)
+            logger.info("Awaiting input..")
+            keyboard.wait()
 
-    CONFIG = load_config()
-    MODE = CONFIG.read_general(c.KEY_MODE).lower()
-
-    if not c.RESOURCES_DIR.exists():
-        c.RESOURCES_DIR.mkdir()
-
-    register_hotkeys()
-
-    PATH_TEMP = TemporaryDirectory()
-    DRIVER = create_driver(PATH_TEMP)
-    path_output = CONFIG.read_path(c.KEY_OUTPUT_DIR)
-
-    log_level = CONFIG.read_general(c.KEY_LOG_LEVEL)
-    logging.getLogger("rich").setLevel(log_level)
-    logging.getLogger("selenium.webdriver.remote.remote_connection").setLevel(log_level)
-
-    if "%USERPROFILE%" in path_output:
-        PATH_OUTPUT = path_output.replace("%USERPROFILE%", os.environ["USERPROFILE"])
-    else:
-        PATH_OUTPUT = path_output
-
-    logger.info(f"Output path set to: {Fore.GREEN}{PATH_OUTPUT}{Fore.WHITE}")
-    logger.info(f"Using mode: {Fore.GREEN}{MODE.title()}{Fore.WHITE}")
-
-    atexit.register(clean_exit)
-
-    sound_theme = CONFIG.read_general(c.KEY_SOUND)
-
-    # TODO log to file
-
-    if sound_theme.lower() != 'none':
-        USE_SOUND = True
-        try:
-            chime.theme(sound_theme)
-        except:
-            logger.warning(f'Unable to read {c.KEY_SOUND}. Defaulting to "big-sur".')
-            chime.theme('big-sur')
-            chime.warning()
-    else:
-        USE_SOUND = False
-        logger.info('Sound has been disabled in config.')
-
-    while True:
-        time.sleep(0.5)
-        logger.info("Awaiting input..")
-        keyboard.wait()
-        time.sleep(0.5)
-
-if __name__ == "__main__":
-    run()
+    except KeyboardInterrupt:
+        logger.info("Quitting due to keyboard interrupt..")
+        return
